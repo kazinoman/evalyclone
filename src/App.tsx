6aea14ca1635026d1ad1921d61ecdfd43bf22ace
abdullah al noman <kazinoman9999@gmail.com>
2023-01-24 01:55:31 +0600

    modify: gitignore file

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 import { Button } from "antd";
@@ -6,15 +6,6 @@ import { Button } from "antd";
 function App() {
   const [count, setCount] = useState(0);
 
-  return (
-    <div className="App">
-      <div className="h-32 w-full border-5 border-white bg-slate-800">asdf</div>
-      <h1 className="ext-primary text-4xl font-bold">
-        Hello vite apps sadf asdf fffff
-      </h1>
-      <Button type="primary">alsdjkhklj</Button>
-    </div>
-  );
+  return <div className="App"></div>;
 }
-
 export default App;
